a343e90338b329b72c6c070058e4510d63681501
Royalito <alejandronavarromorales98@gmail.com>
2023-07-23 09:35:18 -0700

    welcome

--- python-hello_world/6-concat.py
+++ python-hello_world/6-concat.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-str1 = "Welcome to"
-str2 = "Holberton School"
-print(f"{str1} {str2}!\n1\n(1 chars long)")
+str1 = "Welcome to Holberton School"
+print(f"Welcome to {str1}!\n1\n(1 chars long)")

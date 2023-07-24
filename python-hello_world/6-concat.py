f616b8ebbfd72d1fdb045e1baacfd7822032cb13
Royalito <alejandronavarromorales98@gmail.com>
2023-07-24 08:38:12 -0700

    welcome

--- python-hello_world/6-concat.py
+++ python-hello_world/6-concat.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-str1 = "Welcome to "
-str2 = "Holberton School!"
-# Esta variable almacena el texto "Welcome to "
-print(f"{str1}{str2}")
+str1 = "Holberton"
+str2 = "School"
+print(f"Welcome to {str1} {str2}!")

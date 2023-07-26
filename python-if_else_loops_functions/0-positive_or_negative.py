b3dd54b5c5c91fe69592aeee3958155f45685e39
Royalito <alejandronavarromorales98@gmail.com>
2023-07-26 13:11:31 -0700

    negative

--- python-if_else_loops_functions/0-positive_or_negative.py
+++ python-if_else_loops_functions/0-positive_or_negative.py
@@ -4,4 +4,4 @@ import random
 number = random.randint(-10, 10)
 
 # Imprime el número y su estado
-print(number, "is positive" if number > 0 else "is zero" if number == 0 else "is negative")
+print(f"{number} is positive" if number > 0 else f"{number} is zero" if number == 0 else f"{number} is negative")

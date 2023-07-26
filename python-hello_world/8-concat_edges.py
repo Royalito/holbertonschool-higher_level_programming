154f82dadee1d65974b7224b6bf60a06be343980
Royalito <alejandronavarromorales98@gmail.com>
2023-07-26 08:32:31 -0700

    lol

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
-print(str[39:61] + str[-6:-1])
+print((str[39:61] + str[-6:-1]).strip())

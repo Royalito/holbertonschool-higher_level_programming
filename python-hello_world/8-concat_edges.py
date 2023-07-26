3179aa0daacd9b73a5f8d08c261cc5fff3d3ca2e
Royalito <alejandronavarromorales98@gmail.com>
2023-07-26 08:41:25 -0700

    lol

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
-print((str[39:61] + str[-6:-1]).strip())
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+print(str[str.find("object-oriented programming"):str.find("with")+4].strip())

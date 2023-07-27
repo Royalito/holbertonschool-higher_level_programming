74750faaccfc9f2b82f9a3e82d89d180e6975936
Royalito <114327387+Royalito@users.noreply.github.com>
2023-07-27 11:37:57 -0500

    Update 0-positive_or_negative.py

--- python-if_else_loops_functions/0-positive_or_negative.py
+++ python-if_else_loops_functions/0-positive_or_negative.py
@@ -4,4 +4,5 @@ import random
 number = random.randint(-10, 10)
 
 # Imprime el número y su estado
-print(f"{number} is {'positive' if number > 0 else 'zero' if number == 0 else 'negative'}")
+status = "positive" if number > 0 else "zero" if number == 0 else "negative"
+print(f"{number} is {status}")

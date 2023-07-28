29db5973573f1e47ab95a6b66943e0e6eaf0f4b9
Royalito <114327387+Royalito@users.noreply.github.com>
2023-07-28 17:06:25 -0500

    Update 6-print_comb3.py

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 for num1 in range(10):
     for num2 in range(num1 + 1, 10):
-        print("{:02d}, ".format(num1 * 10 + num2), end='')
+        
+       
+print("{:02d}, ".format(num1 * 10 + num2), end='')
 
 print()

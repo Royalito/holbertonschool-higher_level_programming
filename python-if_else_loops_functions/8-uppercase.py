408f5853dcbbdfb3165a3eda37305e4f15890a40
Royalito <114327387+Royalito@users.noreply.github.com>
2023-07-28 17:23:33 -0500

    Update 8-uppercase.py

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,2 +1,5 @@
 #!/usr/bin/python3
-def uppercase(s):print("".join(chr(ord(c)-32)if'a'<=c<='z'else c for c in s))
+def uppercase(s):
+    for c in s:
+        print("{:c}".format(ord(c) - 32 if 'a' <= c <= 'z' else ord(c)), end='')
+    print()

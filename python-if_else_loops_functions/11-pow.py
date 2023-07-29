cda881915d4d20ee7baa88b1fa0cfafbfdcb1d27
Royalito <114327387+Royalito@users.noreply.github.com>
2023-07-29 10:56:57 -0500

    Update 11-pow.py

--- python-if_else_loops_functions/11-pow.py
+++ python-if_else_loops_functions/11-pow.py
@@ -1,14 +1,8 @@
 #!/usr/bin/python3
-def power(a, b):
-    result = 1
+def pow(a, b):
+    resultado = 1
     for _ in range(abs(b)):
-        result *= a
+        resultado *= a
     if b < 0:
-        result = 1 / result
-    return result
-
-# Test cases
-print("Correct output - case: 98 with return:", power(7, 2))
-print("Correct output - case: 98:", power(-7, 2))
-print("Correct output - case: -98 with return:", power(7, -2))
-print("Correct output - case: 0:", power(0, 5))
+        resultado = 1 / resultado
+    return resultado

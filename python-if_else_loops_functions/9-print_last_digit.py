f1b0b56aef21be3b66097f6fdc4f8a9babe38a76
Royalito <114327387+Royalito@users.noreply.github.com>
2023-07-28 20:50:42 -0500

    Update 9-print_last_digit.py

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-def uppercase(s):
-    for c in s:
-        print("{:c}".format(ord(c) - 32 if 'a' <= c <= 'z' else ord(c)), end='')
-    print()
+def print_last_digit(number):
+    last_digit = abs(number) % 10
+    print(last_digit)

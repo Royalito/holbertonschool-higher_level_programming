a6f4d6cfc2c4d686e98ede013dc3f9efc0c4d3a7
Royalito <114327387+Royalito@users.noreply.github.com>
2023-07-29 11:14:18 -0500

    Update 9-print_last_digit.py

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def print_last_digit(number):
     last_digit = abs(number) % 10
-    print("{:d}".format(last_digit))
+    print(last_digit)
 
-print_last_digit(98)
-print_last_digit(0)
-r = print_last_digit(-1024)
-print(r)
+# Ejemplo de uso
+print_last_digit(8044)

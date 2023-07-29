e45d0374a0ebac009c25a4394d5630b617b87579
Royalito <114327387+Royalito@users.noreply.github.com>
2023-07-28 21:10:37 -0500

    Update 11-pow.py

--- python-if_else_loops_functions/11-pow.py
+++ python-if_else_loops_functions/11-pow.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def pow(a, b):
-    result = 1
-    for _ in range(b):
-        result *= a
-    return result
+    resultado = 1
+    for _ in range(abs(b)):
+        resultado *= a
+    if b < 0:
+        resultado = 1 / resultado
+    return resultado

1ac718ad48bb777ea564ebf3c2081618e31b1fdd
Royalito <114327387+Royalito@users.noreply.github.com>
2023-07-31 10:39:09 -0500

    Update 2-args.py

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
+
 if __name__ == '__main__':
     import sys
-    if (len(sys.argv) - 1) != 1:
-        print("{} arguments".format(len(sys.argv) - 1), end='')
+
+    num_arguments = len(sys.argv) - 1
+
+    if num_arguments == 0:
+        print("Number of argument(s): 0.")
+    elif num_arguments == 1:
+        print("Number of argument(s): 1:")
     else:
-        print("{} argument".format(len(sys.argv) - 1), end='')
-    if len(sys.argv) == 1:
-        print(".")
-    else:
-        print(":")
-    for count, argument in enumerate(sys.argv):
-        if (count == 0):
-            continue
-        print("{}: {}".format((count), argument))
+        print("Number of argument(s): {}:".format(num_arguments))
+
+    for idx, arg in enumerate(sys.argv[1:], start=1):
+        print("{}: {}".format(idx, arg))

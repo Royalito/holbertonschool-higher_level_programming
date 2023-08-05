5e6a7e450c6a6eae6e39e67092fa0f2b885dd06a
Royalito <114327387+Royalito@users.noreply.github.com>
2023-08-05 17:47:05 -0500

    Update 8-concat_edges.py

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-print(str[str.find("object-oriented programming"):str.find("with")+4].strip())
+texto = "object-oriented programming with Python"
+print (texto)
